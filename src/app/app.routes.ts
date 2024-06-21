5bce16c48d721925d4fd3b9ce6a3258ce987e7d4
Juan Fajardo <juanfar.group@gmail.com>
2024-06-21 23:23:53 +0200

    feat: Tournament main component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { TournamentsComponent } from './features/tournaments/tournaments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tournaments',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tournaments',
+    loadComponent: () =>
+      import('./features/tournaments/tournaments.component').then(
+        (m) => TournamentsComponent
+      ),
+  },
+];
